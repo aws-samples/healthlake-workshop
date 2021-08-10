0a2b32da397945367681aa38243a7a6e49d1ebc9
Bakha Nurzhanov <bakha@amazon.com>
2021-08-10 15:50:24 -0700

    Add shell script to split large files
    Increase patient population from 2000 to 5000

---
+++ generate_synthetic_data.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+./run_synthea -c synthea.properties -m congestive_heart_failure:breast_cancer -p 5000 -s 0 -cs 0 -r 20210401
+
+find ./output -name "*.ndjson" -size +50000000c -print | while read file; do
+  fn=$(basename -s .ndjson ${file})
+  dn=$(dirname ${file})
+  split --additional-suffix=.ndjson --elide-empty-files --line-bytes=50000000 --numeric-suffixes --suffix-length=2 $file ${dn}/${fn}. && rm $file
+done
